cf8043b3a25bb4d980a0d7499b79f51893acf8e0
Vijayan Rajan <vijayan.rajan@gmail.com>
2017-04-28 10:55:18 -0700

    Added gdb, defaults for gcc, clang

--- setup-common.sh
+++ setup-common.sh
@@ -5,7 +5,9 @@ apt-get install -y \
     clang++-4.0 \
     clang-4.0 \
     g++-6 \
-    gcc-6
+    gcc-6 \
+    gdb \
+    valgrind
 
 apt-get install -y \
     autoconf \
@@ -35,6 +37,11 @@ apt-get install -y \
 apt-get clean
 apt-get autoremove
 rm -rf /var/lib/apt/lists/*
+
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-4.0 400
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-4.0 400
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 600
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-6 600
 
 groupadd -g 1000 docker
 useradd  -g 1000 -u 1000 -m -G users,staff docker
